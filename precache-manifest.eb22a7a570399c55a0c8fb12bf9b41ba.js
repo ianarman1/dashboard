ac8c1d3886d1e95336478f66eb82bfcf2cd00dab
root <root@playlist.connect>
2021-01-16 04:03:14 -0500

    first commit

---
+++ precache-manifest.eb22a7a570399c55a0c8fb12bf9b41ba.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e4086527912979588279121f4fa2c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a76b27c5f3a41fd1dd4",
+    "url": "/static/css/main.ff4aa105.chunk.css"
+  },
+  {
+    "revision": "ac2b300e14366ceb6eb8",
+    "url": "/static/js/2.a5e2e1d8.chunk.js"
+  },
+  {
+    "revision": "b8ca84c5286f38563fca18e993ce3767",
+    "url": "/static/js/2.a5e2e1d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a76b27c5f3a41fd1dd4",
+    "url": "/static/js/main.2b46a3b0.chunk.js"
+  },
+  {
+    "revision": "c1c186720f64570a35f9",
+    "url": "/static/js/runtime-main.7fd143fa.js"
+  },
+  {
+    "revision": "5468e8ae287495d52508a13cbcb7b360",
+    "url": "/static/media/MyriadProBold.5468e8ae.ttf"
+  },
+  {
+    "revision": "b9b1984ce047f493aa952d0094ec49d5",
+    "url": "/static/media/MyriadProSemibold.b9b1984c.ttf"
+  },
+  {
+    "revision": "96ef6d28a76e3750e42636af8b072f9c",
+    "url": "/static/media/artist-square-large.96ef6d28.svg"
+  },
+  {
+    "revision": "f69ab5bf046dca5ae86a9c234112d583",
+    "url": "/static/media/artist-square-small.f69ab5bf.svg"
+  },
+  {
+    "revision": "c05a37842aa586f9ded9c78c91b5f8a3",
+    "url": "/static/media/artist.c05a3784.svg"
+  },
+  {
+    "revision": "d66c8dc37259d97af295becf8d4c2ba2",
+    "url": "/static/media/avatar.d66c8dc3.jpg"
+  },
+  {
+    "revision": "f01aebbc7fb264e81bc6ae512c4362c5",
+    "url": "/static/media/calendar-release-date.f01aebbc.svg"
+  },
+  {
+    "revision": "98044609251094e3de4502dd9e67c044",
+    "url": "/static/media/dislike-icon.98044609.svg"
+  },
+  {
+    "revision": "f83b57e091443f2e0357cdbb4776bca2",
+    "url": "/static/media/down_arrow.f83b57e0.svg"
+  },
+  {
+    "revision": "7ca7479c63d69692feed5877ddcd13a1",
+    "url": "/static/media/hamburger.7ca7479c.svg"
+  },
+  {
+    "revision": "fb15258aededc220243928bd502aa976",
+    "url": "/static/media/instruments.fb15258a.svg"
+  },
+  {
+    "revision": "f8857a896d8b518685333b484f0e602e",
+    "url": "/static/media/kebab-icon.f8857a89.svg"
+  },
+  {
+    "revision": "090ab97e30e173754bdcf49026ab6819",
+    "url": "/static/media/large-profile-circle.090ab97e.svg"
+  },
+  {
+    "revision": "f56b457a38da82093536161d04c9394d",
+    "url": "/static/media/like-icon.f56b457a.svg"
+  },
+  {
+    "revision": "10103135ba2bfedf248e86ba3639f23c",
+    "url": "/static/media/next-song-icon.10103135.svg"
+  },
+  {
+    "revision": "25aca17acf7bd62d9583f5f98e9fb511",
+    "url": "/static/media/remote.25aca17a.PNG"
+  },
+  {
+    "revision": "b488b23a46096fc7efc8059898139226",
+    "url": "/static/media/song-number-circle.b488b23a.svg"
+  },
+  {
+    "revision": "f6c77e9e8bfc14300577f58aaac73a74",
+    "url": "/static/media/theatre.f6c77e9e.svg"
+  }
+]);
